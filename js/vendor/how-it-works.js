6b5dde5f3dfe2190157eda18f0201aae0cacc0b9
madhavendara <50510155+madhavendara@users.noreply.github.com>
2020-09-14 16:10:23 +0530

    steps javascript added

--- js/vendor/how-it-works.js
+++ js/vendor/how-it-works.js
@@ -13,10 +13,71 @@ gsap.from(".how-it-steps", {
 gsap.to(".how-it-steps", {
     scrollTrigger: {
         trigger : '#how-it-works-vendor',
-        scrub : 0.4,
+        scrub : true,
         start : '+=50',
         end: `bottom bottom`,
         pin : '.how-it-steps'
     },
-    bottom : '0%'
+    motionPath:  [
+      {x : '0%', y :'5%'}, 
+      {x : '-15%', y :'35%'}, 
+      {x : '-35%', y :'90%'}, 
+      {x : '-90%', y :'300%'}, 
+      {x : '0%', y :'600%'}, 
+    ],
+    ease: "none"
   })
+
+
+  var i = 0;
+
+  gsap.to($('.how-it-steps').children().eq(i), {
+  scrollTrigger: {
+      trigger : '#how-it-works-vendor',
+      scrub : true,
+      start : '+=' + (200),
+      marker : true,
+      end : '+=' + (500),
+  },
+  motionPath : [
+    {opacity : '1'}, 
+    {opacity : '0'}
+  ],
+  ease: "none",
+  onComplete : good    
+})
+
+function good(){
+  if(i < 5)
+  {
+    i+=1;
+  gsap.to($('.how-it-steps').children().eq(i), {
+    scrollTrigger: {
+        trigger : '#how-it-works-vendor',
+        scrub : true,
+        start : '+=' + (100 + 350*i),
+        marker : true,
+        end : '+=' + (300 + 325*i),
+    },
+    motionPath : [
+      {opacity : '1'}, 
+      {opacity : '0'}
+    ],
+    ease: "none",
+    onComplete : good    
+  })
+
+  console.log('good');
+  }
+
+  else
+  {
+    return false;
+  }
+}
+
+good();
+
+
+
+  // top points : -14%,10%,35%,59%,85% 
